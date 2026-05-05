86ad62da8a3c1b504aa96dd4c699118001374bf9
nanliupython <66490644+nanliupython@users.noreply.github.com>
2026-05-05 16:21:43 +0000

    Deploying to gh-pages from @ nanliupython/nan-liu.com@287bde06c0a6f35cb3c7184e47f4587ad4dac164 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,8 +45,8 @@ ninja.data = [{
             window.location.href = "/nan-liu.com/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/nan-liu.com/cv/";
