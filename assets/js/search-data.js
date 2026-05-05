03f196cff62595eb7bd7685353f26bdf209d6bad
nanliupython <66490644+nanliupython@users.noreply.github.com>
2026-05-05 01:24:50 +0000

    Deploying to gh-pages from @ nanliupython/nan-liu.com@89230c5cb9426c1ff7ccb4a0419b6e1fc006ce4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/nan-liu.com/projects/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/nan-liu.com/news/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
