04a13f388dedebf7be358c78129461320449799f
nanliupython <66490644+nanliupython@users.noreply.github.com>
2026-05-08 14:35:34 +0000

    Deploying to gh-pages from @ nanliupython/nan-liu.com@187a61f8cade6590d44206153efb22d93510d2a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/nan-liu.com/publications/";
           },
         },{id: "nav-news",
-          title: "news",
+          title: "News",
           description: "",
           section: "Navigation",
           handler: () => {
