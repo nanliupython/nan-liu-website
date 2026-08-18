d5c327d69be556fef8188f7ada66bb1ec414b261
nanliupython <66490644+nanliupython@users.noreply.github.com>
2026-08-18 14:18:57 +0000

    Deploying to gh-pages from @ nanliupython/nan-liu-website@16af15eb835e30ffd10e1fee26175e158f7969e5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nanliupython/nan-liu.com",
+    "data-repo": "nanliupython/nan-liu-website",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
